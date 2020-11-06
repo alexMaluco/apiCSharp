02e0084de5eeae50881bee59f57327cfdef0f48c
Alex Barbosa <alex.barbosa.r@gmail.com>
2020-11-06 01:35:49 -0300

    Retirada de linha

--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,5 @@ namespace challengeSeniorUp.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
     }
 }
